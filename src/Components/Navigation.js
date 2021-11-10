9c8578db96c4c993351483f85f19c1bf7ed04e8e
TuanLeAnh1003 <19520331@gm.uit.edu.vn>
2021-11-10 23:56:03 +0700

    fix nav

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,35 +3,6 @@ import './Navigation.css'
 import { Link } from 'react-router-dom'
 
 function Navigation() {
-    const handleHover1 = (e) => {
-        const listHover = document.querySelector('#nav-item-link-book');
-        
-        listHover.style.display = 'block'
-
-        e.target.classList.add('nav-item-link-active')
-    }
-    const handleLeave1 = (e) => {
-        const listHover = document.querySelector('#nav-item-link-book');
-        
-        listHover.style.display = 'none'
-
-        e.target.classList.remove('nav-item-link-active')
-    }
-    const handleHover2 = (e) => {
-        const listHover = document.querySelector('#nav-item-link-statistical');
-        
-        listHover.style.display = 'block'
-
-        e.target.classList.add('nav-item-list-item-active')
-    }
-    const handleLeave2 = (e) => {
-        const listHover = document.querySelector('#nav-item-link-statistical');
-        
-        listHover.style.display = 'none'
-
-        e.target.classList.remove('nav-item-list-item-active')
-    }
-
     return (
         <div className="nav">
             <ul className="nav-wrapper">
@@ -41,12 +12,12 @@ function Navigation() {
                 <li className="nav-item">
                     <Link to="/" className="nav-item-link">Sách</Link>
                 </li>
-                <li className="nav-item" onMouseEnter={e => handleHover1(e)} onMouseLeave={e => handleLeave1(e)}>
+                <li className="nav-item">
                     <Link to="/" className="nav-item-link">
                         Phiếu
                         <i className="nav-item-link-icon fas fa-chevron-down"></i>
                     </Link>
-                    <ul className="nav-item-list"  id="nav-item-link-book">
+                    <ul className="nav-item-list" id="nav-item-link-book">
                         <li className="nav-item-list-item">
                             <Link to="/" className="nav-item-list-item-link">Mượn sách</Link>
                         </li>
@@ -61,7 +32,7 @@ function Navigation() {
                 <li className="nav-item">
                     <Link to="/" className="nav-item-link">Người dùng</Link>
                 </li>
-                <li className="nav-item" onMouseEnter={e => handleHover2(e)} onMouseLeave={e => handleLeave2(e)}>
+                <li className="nav-item">
                     <Link to="/" className="nav-item-link">
                         Thống kê
                         <i className="nav-item-link-icon fas fa-chevron-down"></i>
